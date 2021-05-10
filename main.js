18bf1c5880d7f5c11af79149d717cadae588eafe
Precious Okafor <preciousrobinsonokafor@gmail.com>
2021-05-10 22:19:26 +0100

    Added comment

--- main.js
+++ main.js
@@ -25,6 +25,7 @@ const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, inval
 
 // Add your functions below:
 //function checking validity of credit card
+// Luhn's algorithm
 function validateCred(cred) {
   var x = cred.slice(); //copies array cred to x
 x.reverse()//reverses the order of the array
